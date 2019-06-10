cc0dde9b6200c4e7ae2546de2bd7843eddd2c682
Eric Solberg <eric@tinkery.io>
2019-06-10 20:49:15 +0000

    Update permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ mkdir -p /var/www/html/storage/cache
 mkdir -p /var/www/html/storage/thumbs
 mkdir -p /var/www/html/storage/tmp
 
-chmod -R 777 /var/www/html/storage
-
 chown -R www-data:www-data /var/www/html/
 
 exec $@
